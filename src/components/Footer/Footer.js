732e4ead443ea6fab958a281f0dd04593d93ac7b
AbiCarsonBrown <142737359+AbiCarsonBrown@users.noreply.github.com>
2023-11-22 20:43:15 +0000

    Create footer and header navigation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,23 @@
 import "./Footer.scss";
 import user from "../../assets/icons/user-solid.svg";
 import list from "../../assets/icons/list-ul-solid.svg";
+import marker from "../../assets/icons/marker-pink.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <img src={list} alt="" className="user" />
-      <img src={user} alt="" className="user" />
+      <nav className="footer__nav">
+        <button className="footer__list-button">
+          <img src={list} alt="" className="footer__list-img" />
+        </button>
+        <NavLink to="/">
+          <img src={marker} alt="" className="footer__marker" />
+        </NavLink>
+        <NavLink to="/profile">
+          <img src={user} alt="" className="footer__profile" />
+        </NavLink>
+      </nav>
     </footer>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,25 @@
 import "./Header.scss";
 import logo from "../../assets/icons/marker-pink.svg";
-import { Link } from "react-router-dom";
+import user from "../../assets/icons/user-solid.svg";
+import list from "../../assets/icons/list-ul-solid.svg";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header__wrapper">
-        <Link to="/" className="header__link">
+        <Link to="/" className="header__logo-link">
           <img src={logo} alt="CoffeeBeen logo" className="header__logo" />
           <h2 className="header__title">CoffeeBeen</h2>
         </Link>
+        <nav className="header__nav">
+          <button className="header__list-button">
+            <img src={list} alt="" className="header__list-img" />
+          </button>
+          <NavLink to="/profile">
+            <img src={user} alt="" className="header__profile" />
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
