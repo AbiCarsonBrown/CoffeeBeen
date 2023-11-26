15886b946781a65d193cc044eadf82881cd1422a
AbiCarsonBrown <142737359+AbiCarsonBrown@users.noreply.github.com>
2023-11-26 20:45:27 +0000

    Add delete review functionality

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -9,6 +9,7 @@ import { customCoffeeBean } from "../../utils/customIcons";
 
 export default function ReviewCard({ review, isUser, submitVisit }) {
   const [edit, setEdit] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
   const [rating, setRating] = useState(review.rating);
 
   const handleRating = (ratingVal) => {
@@ -16,14 +17,19 @@ export default function ReviewCard({ review, isUser, submitVisit }) {
     submitVisit(review.visited, review.on_wishlist, ratingVal, review.review);
   };
 
+  const handleDelete = () => {
+    setIsDelete(false);
+    setRating(null);
+    submitVisit(review.visited, review.on_wishlist, null, null);
+  };
+
   return (
     <article className={`review-card ${edit ? "review-card--edit" : ""}`}>
       {!isUser && <p className="review-card__username">{review.username}</p>}
-
       {isUser && (
         <div className="review-card__header">
           <p className="review-card__user">You</p>
-          {review.review && !edit && (
+          {review.review && !edit && !isDelete && (
             <div className="review-card__actions">
               <button
                 onClick={() => {
@@ -32,64 +38,92 @@ export default function ReviewCard({ review, isUser, submitVisit }) {
                 className="review-card__button review-card__button--edit">
                 <Edit className="review-card__icon" />
               </button>
-              <button className="review-card__button review-card__button--delete">
+              <button
+                onClick={() => {
+                  setIsDelete(true);
+                }}
+                className="review-card__button review-card__button--delete">
                 <Delete className="review-card__icon" />
               </button>
             </div>
           )}
         </div>
       )}
-
-      {!isUser && (
+      {!isDelete && (
         <>
-          <Rating
-            readOnly
-            style={{ maxWidth: 100 }}
-            value={review.rating}
-            itemStyles={customCoffeeBean}
-            spaceBetween="none"
-            spaceInside="none"
-            className="review-card__rating"
-          />
-          <p className="review-card__review">{review.review}</p>
-        </>
-      )}
-
-      {isUser && (
-        <>
-          <Rating
-            style={{ maxWidth: 100 }}
-            value={rating}
-            onChange={handleRating}
-            itemStyles={customCoffeeBean}
-            spaceBetween="none"
-            spaceInside="none"
-            className="review-card__rating"
-          />
-          {!edit && (
+          {!isUser && (
             <>
-              {review.review && (
-                <p className="review-card__review">{review.review}</p>
-              )}
-              {!review.review && (
-                <button
-                  onClick={() => {
-                    setEdit(true);
-                  }}
-                  className="review-card__new">
-                  Leave your review
-                </button>
-              )}{" "}
+              <Rating
+                readOnly
+                style={{ maxWidth: 100 }}
+                value={review.rating}
+                itemStyles={customCoffeeBean}
+                spaceBetween="none"
+                spaceInside="none"
+                className="review-card__rating"
+              />
+              <p className="review-card__review">{review.review}</p>
             </>
           )}
-          {edit && (
-            <ReviewForm
-              review={review}
-              submitVisit={submitVisit}
-              setEdit={setEdit}
-            />
+
+          {isUser && (
+            <>
+              <Rating
+                style={{ maxWidth: 100 }}
+                value={rating}
+                onChange={handleRating}
+                itemStyles={customCoffeeBean}
+                spaceBetween="none"
+                spaceInside="none"
+                className="review-card__rating"
+              />
+              {!edit && (
+                <>
+                  {review.review && (
+                    <p className="review-card__review">{review.review}</p>
+                  )}
+                  {!review.review && (
+                    <button
+                      onClick={() => {
+                        setEdit(true);
+                      }}
+                      className="review-card__new">
+                      Leave your review
+                    </button>
+                  )}{" "}
+                </>
+              )}
+              {edit && (
+                <ReviewForm
+                  review={review}
+                  submitVisit={submitVisit}
+                  setEdit={setEdit}
+                />
+              )}
+            </>
           )}
         </>
+      )}
+      {isDelete && (
+        <div className="review-card__delete">
+          <p className="review-card__delete-check">
+            Are you sure you want to delete your rating & review?
+          </p>
+          <div className="review-card__delete-buttons">
+            <button
+              onClick={() => {
+                setIsDelete(false);
+              }}
+              className="review-card__delete-button">
+              Cancel
+            </button>
+            <button
+              onClick={handleDelete}
+              className="review-card__delete-button review-card__delete-button--confirm">
+              Confirm
+            </button>
+          </div>
+        </div>
       )}
     </article>
   );
