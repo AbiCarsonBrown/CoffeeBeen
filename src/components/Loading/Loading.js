19fc9e4ec35386ed16308b13a2f2623aaeff1cd3
AbiCarsonBrown <142737359+AbiCarsonBrown@users.noreply.github.com>
2024-04-02 23:52:32 +0100

    Style loading page

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,17 @@
+import "./Loading.scss";
+
 export default function Loading() {
   return (
     <main className="loading">
-      <h1 className="loading__title">Loading...</h1>
-      <p className="loading__content">
-        Please be patient while our server wakes up
-      </p>
+      <div className="loading__wrapper">
+        <h1 className="loading__title">Get your mugs ready...</h1>
+        <p className="loading__content loading__content--brewing">
+          Our server is waking up and brewing some fresh data.
+        </p>
+        <p className="loading__content loading__content--caffeine">
+          Just hang tight while we pour in some digital caffeine!
+        </p>
+      </div>
     </main>
   );
 }
